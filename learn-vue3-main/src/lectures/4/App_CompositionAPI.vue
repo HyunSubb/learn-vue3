7eb5bc31129bdb3f71ab5542368338b11b859970
신현섭 <didakd22@naver.com>
2026-02-10 00:52:58 +0900

    커밋

--- learn-vue3-main/src/lectures/4/App_CompositionAPI.vue
+++ learn-vue3-main/src/lectures/4/App_CompositionAPI.vue
@@ -32,6 +32,7 @@ export default {
     // onMounted, onBeforeMount는 라이프 사이클 훅은 Vue 프레임워크가 자동으로 실행해 주는 함수들이다.
     // create → mount → update → destroy 대충 이런 흐름으로 라이프사이클이 진행됨.
     // Vue 인스턴스는 크게 생성(create)되고, DOM에 부착(mount)되고, 업데이트(update)되며, 없어지는(destroy) 4가지 과정을 거치게 된다.
+    // 라이프사이클 단계에서 실행되는 함수를 라이프사이클 훅이라고 부른다.
     onMounted(() => {
       console.log("onMounted");
     });
