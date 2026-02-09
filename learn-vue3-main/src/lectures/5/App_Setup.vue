84a54a9197805b62f07a5fc5f86851021095de11
신현섭 <didakd22@naver.com>
2026-02-10 00:58:51 +0900

    커밋

--- learn-vue3-main/src/lectures/5/App_Setup.vue
+++ learn-vue3-main/src/lectures/5/App_Setup.vue
@@ -1,31 +1,33 @@
 <template>
-	<div>
-		<p>
-			{{ counter }}
-		</p>
-		<p>
-			{{ message }}
-		</p>
-		<button @click="increment">click!</button>
-	</div>
+  <div>
+    <p>
+      {{ counter }}
+    </p>
+    <p>
+      {{ message }}
+    </p>
+    <button @click="increment">click!</button>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
-	setup() {
-		const counter = ref(0);
-		const message = ref('Hello Vue3');
-		const increment = () => {
-			counter.value++;
-		};
-		return {
-			counter,
-			message,
-			increment,
-		};
-	},
+  setup() {
+    // setup() 함수(hook)는 Composition API 사용을 위한 진입점 역할을 한다.
+    // 그리고 setup 함수는 컴포넌트 인스턴스가 생성되기 전에 실행된다.
+    const counter = ref(0);
+    const message = ref("Hello Vue3");
+    const increment = () => {
+      counter.value++;
+    };
+    return {
+      counter,
+      message,
+      increment,
+    };
+  },
 };
 </script>
 
