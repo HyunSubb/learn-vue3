63211597400b6126f6b73576bbb94f4a0c62408c
신현섭 <didakd22@naver.com>
2026-02-10 01:26:05 +0900

    커밋

--- learn-vue3-main/src/lectures/6/App_TemplateSyntax.vue
+++ learn-vue3-main/src/lectures/6/App_TemplateSyntax.vue
@@ -1,48 +1,54 @@
 <template>
-	<div>
-		<h2>보간법</h2>
-		<p>{{ message }}</p>
-		<p v-once>{{ message }}</p>
-		<button v-on:click="message = message + '!'">Click!</button>
-		<hr />
-		<h2>HTML</h2>
-		<p>{{ rawHtml }}</p>
-		<p v-html="rawHtml"></p>
-		<hr />
-		<h2>속성 바인딩</h2>
-		<div :title="dynamicTitle">마우스를 올려보세요</div>
-		<input type="text" value="홍길동" :disabled="isInputDisabled" />
-		<input v-bind="attrs" />
-		<h2>JavaScript</h2>
-		{{ message.split('').reverse().join('') }}<br />
-		{{ isInputDisabled ? '예' : '아니오' }}
-	</div>
+  <div>
+    <h2>보간법</h2>
+    <!-- Vue는 템플릿 문법을 사용하여 렌더링된 DOM을 컴포넌트의 인스턴스 데이터에 선언적으로 바인딩할 수 있다. -->
+    <p>{{ message }}</p>
+    <!-- v-once 디렉티브를 사용하여 데이터가 변경되어도 갱신(반응)되지 않는 일회성 보간을 수행할 수 있다. -->
+    <p v-once>{{ message }}</p>
+    <button v-on:click="message = message + '!'">Click!</button>
+    <hr />
+    <h2>HTML</h2>
+    <p>{{ rawHtml }}</p>
+    <p v-html="rawHtml"></p>
+    <hr />
+    <h2>속성 바인딩</h2>
+    <!-- 이중 중괄호는 HTML 속성에 사용할 수 없습니다. 대신 [v-bind]를 사용하세요. -->
+    <!-- v-bind는 매우 자주 사용하기 때문에 단축 문법(:)이 있습니다. -->
+    <div :title="dynamicTitle">마우스를 올려보세요</div>
+    <input type="text" value="홍길동" :disabled="isInputDisabled" />
+    <!-- 다중 속성 바인딩 : 여러 속성을 한번에 바인딩할 수 있다. -->
+    <input v-bind="attrs" />
+    <h2>JavaScript</h2>
+    <!-- 이중 괄호에서는 자바스크립트의 표현식도 사용할 수 있다. -->
+    {{ message.split("").reverse().join("") }}<br />
+    {{ isInputDisabled ? "예" : "아니오" }}
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
-	setup() {
-		const message = ref('안녕하세요!');
-		const rawHtml = ref('<strong>안녕하세요</strong>');
+  setup() {
+    const message = ref("안녕하세요!");
+    const rawHtml = ref("<strong>안녕하세요</strong>");
 
-		const dynamicTitle = ref('안녕하세요!@@@@@!!!');
-		const isInputDisabled = ref(false);
-		const attrs = ref({
-			type: 'text',
-			value: '12345678',
-			disalbed: false,
-		});
+    const dynamicTitle = ref("안녕하세요!@@@@@!!!");
+    const isInputDisabled = ref(false);
+    const attrs = ref({
+      type: "text",
+      value: "12345678",
+      disalbed: false,
+    });
 
-		return {
-			message,
-			rawHtml,
-			dynamicTitle,
-			isInputDisabled,
-			attrs,
-		};
-	},
+    return {
+      message,
+      rawHtml,
+      dynamicTitle,
+      isInputDisabled,
+      attrs,
+    };
+  },
 };
 </script>
 
--- learn-vue3-main/src/main.js
+++ learn-vue3-main/src/main.js
@@ -1,18 +1,46 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { createApp } from 'vue';
-import App from './App.vue';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createApp } from "vue";
+// import App from './App.vue';
+
+// --- Lecture App Imports (Uncomment to use) ---
+// import App from './lectures/3/App_CompositionAPI.vue';
+// import App from './lectures/3/App_OptionsAPI.vue';
+// import App from './lectures/4/App_CompositionAPI.vue';
+// import App from './lectures/5/App_Setup.vue';
+import App from "./lectures/6/App_TemplateSyntax.vue";
+// import App from './lectures/7/App_reactive.vue';
+// import App from './lectures/7/App_readonly.vue';
+// import App from './lectures/7/App_ref_object_array.vue';
+// import App from './lectures/7/App_ref.vue';
+// import App from './lectures/7/App_toRefs_toRef.vue';
+// import App from './lectures/8/App_computed.vue';
+// import App from './lectures/9/App_class_binding.vue';
+// import App from './lectures/9/App_style_binding.vue';
+// import App from './lectures/10/App_v-if.vue';
+// import App from './lectures/11/App_v-for.vue';
+// import App from './lectures/12/App_directives.vue';
+// import App from './lectures/12/App_v-memo.vue';
+// import App from './lectures/13/App_event.vue';
+// import App from './lectures/13/App_keyboard.vue';
+// import App from './lectures/13/App_mouse.vue';
+// import App from './lectures/13/App.vue';
+// import App from './lectures/14/App_two_way_binding.vue';
+// import App from './lectures/15/App_immediate.vue';
+// import App from './lectures/15/App_multiple_source_type.vue';
+// import App from './lectures/15/App_watch.vue';
+// import App from './lectures/15/App_watcheffect.vue';
 
 //  <!-- 그리고 이 안에는 루트 컴포넌트로 렌더링이 될 App.vue가 들어가게 된다. -->
 const app = createApp(App);
 
 // app.component('AppCard', AppCard);
 
-app.provide('app-message', 'app message 입니다');
+app.provide("app-message", "app message 입니다");
 //
-app.config.globalProperties.msg = 'hello';
-app.provide('msg', 'hello msg');
+app.config.globalProperties.msg = "hello";
+app.provide("msg", "hello msg");
 // <!-- 그런 다음 생성된 뷰 인스턴스를 마운트함. 결론적으로 이 안에 있는 루트 컴포넌트가 렌더링 된다고 보면 된다. -->
 // #app은 index.html에 있는 id="app"을 의미.
 // 결론적으로 index.html에 있는 id="app" 태그 안에 App.vue(=루트 컴포넌트)가 렌더링 된다고 보면 된다.
-app.mount('#app');
-import 'bootstrap/dist/js/bootstrap.js';
+app.mount("#app");
+import "bootstrap/dist/js/bootstrap.js";
