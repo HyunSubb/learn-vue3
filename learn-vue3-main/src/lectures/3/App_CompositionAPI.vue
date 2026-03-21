7769949f54f68794e16d4c07a3e7e15efad154ce
신현섭 <didakd22@naver.com>
2026-03-21 13:50:55 +0900

    커밋

--- learn-vue3-main/src/lectures/3/App_CompositionAPI.vue
+++ learn-vue3-main/src/lectures/3/App_CompositionAPI.vue
@@ -8,10 +8,15 @@
 import { onMounted, ref } from 'vue';
 
 export default {
-	setup() {
+  setup() {
+    // Composition API를 사용하면 동일한 논리적 관심사 코드가 그룹화 되어 코드를 분석하기도 쉽고 유지보수가 용이해진다.
 		const counter = ref(0);
+    const increment = () => counter.value++;
 
-		const increment = () => counter.value++;
+    const books = reactive([]);
+    const addBook = (title, author) => {
+      books.push({ title, author });
+    };
 
 		onMounted(() => {
 			console.log('컴포넌트가 마운트 되었습니다!');
@@ -19,7 +24,8 @@ export default {
 
 		return {
 			counter,
-			increment,
+      increment,
+      addBook,
 		};
 	},
 };
--- learn-vue3-main/src/lectures/7/_02_App_ref.vue
+++ learn-vue3-main/src/lectures/7/_02_App_ref.vue
@@ -10,14 +10,17 @@ import { reactive, ref } from "vue";
 
 export default {
   setup() {
+    // ref는 primitive 타입을 반응형으로 만들 때 사용한다.
     let message = ref("Hello Vue!");
+
     // 밑에 reactive로 선언된것은 반응형으로 동작하지 않는데 reactive() 함수는 객체타입에만 동작하기 때문.
     // let message = reactive("Hello Vue!");
 
     const addMessage = () => {
       message.value = message.value + "!";
     };
-    console.log(message);
+    console.log("message: ", message);
+    console.log("message typeof: ", typeof message);
     console.log("message: ", message.value);
     console.log("message typeof: ", typeof message.value);
     return {
